12c547ba327f7436a45f277c6c1edf56b974939a
SomneelMaity <somneel.maity@gmail.com>
2023-12-12 22:21:47 +0530

    Updating State Based on Old State Correctly

--- TicTacToe/src/components/Player.jsx
+++ TicTacToe/src/components/Player.jsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 export default function Player({name, symbol}) {
     const [ isEditing, setIsEditing ] = useState(false);
 
-    const handleEditClick = (e) => {
-        setIsEditing(true);
+    const handleEditClick = () => {
+        setIsEditing(editing => !editing);
     }
 
     let playerName = <span className="player-name">{name}</span>;
+    // let btnCaption = "Edit";
     if(isEditing) {
-        playerName = <input type="text" required />
+        playerName = <input type="text" required value={name} />
+        // btnCaption="Save";
     }
 
     return (
@@ -17,7 +19,7 @@ export default function Player({name, symbol}) {
           {playerName}
           <span className="player-symbol">{symbol}</span>
         </span>
-        <button onClick={handleEditClick}>Edit</button>
+        <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
       </li>
     );
 }
